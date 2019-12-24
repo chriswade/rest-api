3611fd812b8d7de0c8de064a88d75b3b66025113
Christopher Wade <chris.wade@comparethemarket.com.au>
2019-12-24 14:39:57 +1000

    db stuff

--- database/database.go
+++ database/database.go
@@ -4,6 +4,7 @@ import (
 	"database/sql"
 	"fmt"
 
+	"github.com/chriswade/rest-api/data"
 	_ "github.com/lib/pq"
 )
 
@@ -25,12 +26,25 @@ func Connect() {
 	}
 	defer db.Close()
 
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Successfully connected!")
+}
+
+func Add(item data.Book) {
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+	db, err := sql.Open("postgres", psqlInfo)
 	sqlStatement := `
 	INSERT INTO users (age, email, first_name, last_name)
-	VALUES ($1, $2, $3, $4);
+	VALUES ($1, $2, $3, $4)
 	RETURNING id`
 	id := 0
-	err = db.QueryRow(sqlStatement, 29, "hi@christopherwa.de", "Christopher", "Wade").Scan(&id)
+	err = db.QueryRow(sqlStatement, 21, "lee@we.com", "Lee", "We").Scan(&id)
 	if err != nil {
 		panic(err)
 	}
